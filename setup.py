8d3240f2e6d82c1be4ab35ce06d68cd8523cbdd5
cloverrose <clover.plus.rose@gmail.com>
2013-03-24 18:03:34 +0900

    update nowfm then modify

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='nowfm',
-    version='0.0.16',
+    version='0.0.17',
     description='nowfm is a library for get nowplaying track information from Last.fm.',
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
